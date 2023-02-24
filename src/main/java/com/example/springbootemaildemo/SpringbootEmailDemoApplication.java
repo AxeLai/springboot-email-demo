d79949c5c2a1e59ca9818c5221b57e02dda83f13
laixiansong <xiansong.lai@gientech.com>
2023-02-24 19:32:59 +0800

    加个署名

--- src/main/java/com/example/springbootemaildemo/SpringbootEmailDemoApplication.java
+++ src/main/java/com/example/springbootemaildemo/SpringbootEmailDemoApplication.java
@@ -3,6 +3,9 @@ package com.example.springbootemaildemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lai
+ */
 @SpringBootApplication
 public class SpringbootEmailDemoApplication {
 
